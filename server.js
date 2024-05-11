42d68921debe8746703fd38275024440a0930487
centristy <centristy@gmail.com>
2024-05-11 21:27:06 +0900

    server modified

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const app = require("./app");
+const http = require('http')
+
+const port = process.env.PORT || 5000
+const server = http.createServer(app)
+
+server.listen(port, function () {
+  console.log(`Started on http://localhost:${port}`);
+});
