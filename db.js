8c692e5f095af93ea4f014ca9945cefc32f99eb9
centristy <centristy@gmail.com>
2024-05-11 21:40:48 +0900

    database added

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
 "use strict";
-
+/** Database setup for jobly. */
 const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
 
+let db;
 
-let DB_URI;
-
-if(process.env.NODE_ENV === "test"){
-    DB_URI = "jobly_test";
-
-}else{
-    DB_URI = "jobly"
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri()
+  });
 }
-
-let db = new Client({
-  host: "/var/run/postgresql",
-  database: DB_URI
-});
-
 
 db.connect();
 
